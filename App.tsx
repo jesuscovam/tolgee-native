0208cef228be4736ca6fea6d2eb830de092cb5f5
Jesus Cova <jesuscovam@gmail.com>
2022-05-22 13:23:21 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
+import '@expo/browser-polyfill'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { TolgeeProvider } from '@tolgee/react'
+
+
+const REACT_APP_TOLGEE_API_URL = "https://app.tolgee.io" as const
+const REACT_APP_TOLGEE_API_KEY = "demo-please-replace-with-your-token" as const
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TolgeeProvider filesUrlPrefix="i18n/"
+      apiUrl={REACT_APP_TOLGEE_API_URL}
+      apiKey={REACT_APP_TOLGEE_API_KEY}
+      loadingFallback={<ActivityIndicator />}
+    >
+
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TolgeeProvider>
   );
 }
 
